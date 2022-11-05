61ef096e778df747f9edfbcf02f2d9a3d9a2cdb2
brunoivisS <brunoivis13@gmail.com>
2022-11-05 19:30:32 -0300

    ambiente de desenvolmento android instalado

---
+++ src/screens/signin.tsx
@@ -0,0 +1,15 @@
+
+import { Text,Center } from "native-base";
+
+export function Signin(){
+   
+    return (
+        <Center flex={1} bgColor="gray.900" >
+        <Text color="blue.300" fontSize="47" fontFamily="">
+                SignIn 
+          </Text>
+      </Center>
+)
+}
+
+   
